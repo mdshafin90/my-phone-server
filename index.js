48d091c08b09641f05464ffa160d6236105760b3
shafin bisswas <mdnazmussakibshafin@gmail.com>
2023-05-15 22:20:13 +0600

    server ready with nodemon index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 2000
+
+const phones = require('./phones.json');
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+})
+
+app.get('./phones/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log('i need data for id: ', id)
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone)
+})
+
+app.listen(port, () => {
+    console.log(`phone post running: ${port}`)
+})
